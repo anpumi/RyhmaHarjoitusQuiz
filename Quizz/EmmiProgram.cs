94bb5f4e61a221aceebf1d0d7853727c1db577f3
anpumi <annukka.puotiniemi@gmail.com>
2019-01-28 14:03:12 +0200

    Lisätty kahden muun projektin relevantit tiedotot deva:n

---
+++ Quizz/EmmiProgram.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Kysymykset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu;
+            do
+            {
+
+
+
+            //    Console.Clear();
+            //Console.WriteLine("Valikko");
+            //Console.WriteLine("1. Pelaa peli");
+            //Console.WriteLine("2. Lopeta peli");
+            //menu = Console.ReadLine();
+            //Console.Clear();
+
+            ListanLatausJaSekoitus();
+
+
+            string pelaajanNimi;
+            Kysymykset kys = new Kysymykset();
+            Pelaaja print = new Pelaaja();
+                Console.Clear();
+            Console.WriteLine("Hei, kerro nimesi!");
+            pelaajanNimi = Console.ReadLine();
+                Console.Clear();
+
+
+
+            kys.lueKysymykset();
+            int kokonaispisteet = 0;
+            
+
+            kokonaispisteet = kys.kysyKysymys();
+                Console.Clear();
+            Console.WriteLine($"Kiitos pelaamisesta {pelaajanNimi}!");
+
+                if (kokonaispisteet < 4)
+                {
+                    Console.WriteLine($"Sait {kokonaispisteet} pistettä kymmenestä! Heikkoa.");
+                }
+                else if (kokonaispisteet < 8)
+                {
+                    Console.WriteLine($"Sait {kokonaispisteet} pistettä kymmenestä! Hyvä!");
+                }
+                else
+                {
+                    Console.WriteLine($"Sait {kokonaispisteet} pistettä kymmenestä! Hienoa, siirry Afterworkin pariin!");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Valikko");
+                Console.WriteLine("1. Pelaa peli");
+                Console.WriteLine("2. Lopeta peli");
+                menu = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("KIITTIMOI!");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+            } while (menu != "2");
+
+            void ListanLatausJaSekoitus()
+            {
+                List<string> järjestyksessä;
+                using (StreamReader lataus = new StreamReader("c://work//kysymykset.txt", Encoding.GetEncoding("iso-8859-1")))
+                {
+                    järjestyksessä = new List<string>();
+                    string rivi = lataus.ReadLine();
+                    while (rivi != null)
+                    {
+                        järjestyksessä.Add(rivi);
+                        rivi = lataus.ReadLine();
+                    }
+                }
+                Random rng = new Random();
+                string[] randomiksi = järjestyksessä.OrderBy(x => rng.Next()).ToArray();
+                using (StreamWriter tallennus = new StreamWriter("c://work//kysymykset2.txt", false, Encoding.GetEncoding("iso-8859-1")))
+                {
+                    foreach (var r in randomiksi)
+                    {
+                        tallennus.WriteLine(r);
+                    }
+                }
+                return;
+            }
+        }
+    }
+}
---
+++ Quizz/MattiProgram.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MiniProjekti
+{
+    class Program
+    {
+        static string nimi;
+        
+        static void Main(string[] args)
+        {
+
+            Console.Write("Anna nimesi: ");
+            nimi = Console.ReadLine(); 
+            Kysymysgeneraattori();
+        }
+                
+        private static void Kysymysgeneraattori()
+        {
+            
+            List<string[]> kysymykset = new List<string[]>();
+
+            StreamReader sr = new StreamReader(@"C:\Users\erone\Documents\Academy tehtävät\Viikko 1\Viikko1 Miniprojekti\MiniProjekti\kysymykset.CSV", Encoding.GetEncoding("iso-8859-1"));
+
+            string rivi = sr.ReadLine();
+            while (rivi != null)
+            {
+                string[] kysymys = rivi.Split(';');
+                kysymykset.Add(kysymys);
+                rivi = sr.ReadLine();
+            }
+            sr.Close();
+
+
+            List<string[]> randomIndex = new List<string[]>();
+
+            Random rnd = new Random();
+
+            while (randomIndex.Count != 10)
+            {
+                int luku = rnd.Next(0, kysymykset.Count);
+                if (!randomIndex.Contains(kysymykset[luku]))
+                {
+                    randomIndex.Add(kysymykset[luku]);
+                }
+            }
+ 
+
+            int counter = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                
+                Console.WriteLine(randomIndex[i][0]);
+                alku1: Console.WriteLine("Totta vai tarua?");
+                string vastaus = Console.ReadLine();
+                vastaus = vastaus.ToLower();
+                vastaus = vastaus.Trim();
+                if (vastaus != "totta" && vastaus!="tarua")
+                {
+                    Console.WriteLine("Vastaususvaihtoehdot ovat \"totta\"" + " \"tarua\"");
+                    goto alku1;
+                    
+                }
+
+                if (vastaus == randomIndex[i][1])
+                {
+                    Console.WriteLine("Oikein!");
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine("Väärin!");
+                }
+                Console.Clear();
+            }
+
+            string tulos = "";
+            if (counter > 6)
+                tulos = "Hyvin";
+            else if (counter < 2)
+                tulos = "Huonosti";
+            else
+                tulos = "Kohtalaisesti";
+
+            Console.WriteLine($"{tulos} meni {nimi}, pisteesi {counter}/10");
+        }
+    }
+}
